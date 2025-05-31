d9a5174895375e64070a09e0974f19e17941bbfb
Satyabrata Panda <pandasatyabrata566@gmail.com>
2025-05-31 16:23:08 +0530

    New code added

---
+++ rentapp/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from . models import *
+
+# Register your models here.
+admin.site.register(Users)
+admin.site.register(Vehicles)
+admin.site.register(Orders)
+admin.site.register(Contactus)
